11df8892396a4dfa9caa22b40d58f6a77e2c2555
Soumesh Kundu <iamsoumo26@gmail.com>
2023-08-18 14:20:08 +0530

    backend implementing

---
+++ layouts/clean.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="relative px-4 ">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/callback.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout:"clean"
+})
+const {Realm}=useRealm()
+onMounted(()=>{
+    console.log('hello')
+    Realm.handleAuthRedirect()
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
